d8f6359f5f17307631471b003c2170dd88f9e97a
juhavaaraniemi <100115205+juhavaaraniemi@users.noreply.github.com>
2023-11-01 10:29:13 +0200

    Create scales.lua

---
+++ scales.lua
@@ -0,0 +1,216 @@
+-- scaler
+-- v0.1
+
+
+--
+-- LIBRARIES
+--
+engine.name = 'PolySub'
+musicutil = require 'musicutil'
+PolySub = require 'polysub'
+
+
+--
+-- VARIABLES
+--
+playing_notes = {}
+
+--
+-- INIT FUNCTIONS
+--
+function init_parameters()
+  params:add_separator("SCALER")
+  params:add_group("SCALER - SCALES",12)
+  params:add{
+    type="number",
+    id="scale",
+    name="scale",
+    min=1,
+    max=40,
+    default=1,
+    formatter=function(param)
+      return string.lower(musicutil.SCALES[param:get()].name)
+    end,
+    action=function()
+      --max selected note is set to number of notes in scale
+      local note_look = params.lookup["selected_note"]
+      params.params[note_look].max = #musicutil.SCALES[params:get("scale")].intervals
+      params:set("selected_note",1)
+      --max selected chord and value is set first to 0
+      for i=1,9 do
+        local chord_look = params.lookup[i.."selected_chord"]
+        params.params[chord_look].max = 0
+        params:set(i.."selected_chord",0)
+      end
+      --selected chord max is set to possible amount of chords
+      --default chord selection is set to basic triad form
+      for i=1,#musicutil.SCALES[params:get("scale")].intervals do
+        local chord_look = params.lookup[i.."selected_chord"]
+        params.params[chord_look].max = #musicutil.SCALES[params:get("scale")].chords[i]
+        if #musicutil.SCALES[params:get("scale")].chords[i] > 0 then
+          params.params[chord_look].min = 1
+          params:set(i.."selected_chord",1)
+          for key, value in pairs(musicutil.SCALES[params:get("scale")].chords[i]) do
+            print(i.." "..key.." "..value)
+            if value == 1 then
+              params:set(i.."selected_chord",key)
+            elseif value == 12 then
+              params:set(i.."selected_chord",key)
+            elseif value == 17 then
+              params:set(i.."selected_chord",key)
+            elseif value == 24 then
+              params:set(i.."selected_chord",key)
+            end
+          end
+        end
+      end
+    end
+  }
+  params:add{
+    type="number",
+    id="root_note",
+    name="root note",
+    min=0,
+    max=11,
+    default=0,
+    formatter=function(param)
+      return musicutil.note_num_to_name(param:get(),false)
+    end,
+    action=function()
+    end
+  }
+  params:add{
+    type="number",
+    id="selected_note",
+    name="selected note",
+    min=1,
+    max=1,
+    default=1,
+    action=function()
+    end
+  }
+  for i=1,9 do
+    params:add{
+      type="number",
+      id=i.."selected_chord",
+      name=i.." selected chord",
+      min=0,
+      max=0,
+      default=0,
+      action=function()
+      end
+    }
+  end
+  params:add_group("SCALER - POLYSUB",19)
+  PolySub.params()
+  params:bang()
+end
+
+function init()
+  init_parameters()
+  redraw_metro = metro.init(redraw_event, 1/30, -1)
+  redraw_metro:start()
+end
+
+
+--
+-- CLOCK FUNCTIIONS
+--
+function redraw_event()
+  if screen_dirty then
+    redraw()
+    screen_dirty = false
+  end
+end
+
+
+--
+-- AUDIO FUNCTIONS
+--
+function play_chord()
+  chords = musicutil.chord_types_for_note(musicutil.SCALES[params:get("scale")].intervals[params:get("selected_note")]+params:get("root_note"), params:get("root_note"), params:get("scale"))
+  chord_notes = musicutil.generate_chord(musicutil.SCALES[params:get("scale")].intervals[params:get("selected_note")]+params:get("root_note"),chords[params:get(params:get("selected_note").."selected_chord")])
+  
+  for i=1,#chord_notes do
+    table.insert(playing_notes,chord_notes[i])
+    engine.start(chord_notes[i],musicutil.note_num_to_freq(chord_notes[i]+48))
+  end
+  print("playing")
+end
+
+function stop_chord()
+  for i=1,#playing_notes do
+    engine.stop(playing_notes[i])
+  end
+  playing_notes = {}
+  print("stopped playing")
+end
+
+  
+
+--
+-- UI FUNCTIONS
+--
+function key(n,z)
+  if n == 1 then
+    shifted = z == 1
+  elseif n == 2 and z == 1 and shifted then
+    --play_chord()
+  elseif n == 3 and z == 1 and shifted then
+    --stop_chord()
+  elseif n == 2 and z == 1 then
+    --params:set("selected_note",util.clamp(params:get("selected_note") - 1,1,#musicutil.SCALES[params:get("scale")].intervals-1))
+    stop_chord()
+    play_chord()
+  elseif n == 3 and z == 1 then
+    --params:set("selected_note",util.clamp(params:get("selected_note") + 1,1,#musicutil.SCALES[params:get("scale")].intervals-1))
+    stop_chord()
+  end
+  screen_dirty = true
+end
+
+function enc(n,d)
+  if n == 1 then
+    params:delta("scale",d)
+  elseif n == 2 then
+    params:delta("selected_note",d)
+  elseif n == 3 then
+    params:delta(params:get("selected_note").."selected_chord",d)
+  end
+  screen_dirty = true
+end
+
+--
+-- REDRAW FUNCTIONS
+--
+function redraw()
+  screen.clear()
+  screen.level(15)
+  h = 6
+  for i=1,#musicutil.SCALES[params:get("scale")].intervals do
+    --print(musicutil.SCALES[params:get("scale")].intervals[i])
+    screen.move(5,h*i)
+    screen.text(i..":")
+    screen.move(0,h*i)
+    if i == params:get("selected_note") then
+      screen.text("O")
+    end
+    screen.move(15,h*i)
+    screen.text(musicutil.note_num_to_name(musicutil.SCALES[params:get("scale")].intervals[i]+params:get("root_note")), false)
+    chords = musicutil.chord_types_for_note(musicutil.SCALES[params:get("scale")].intervals[i]+params:get("root_note"), params:get("root_note"), params:get("scale"))
+    chord_notes = musicutil.generate_chord(musicutil.SCALES[params:get("scale")].intervals[i]+params:get("root_note"),chords[params:get(i.."selected_chord")])
+    screen.move(27,h*i)
+    --w = 14
+    if params:get(i.."selected_chord") > 0 then
+      screen.text(chords[params:get(i.."selected_chord")])
+    --  for j=2,#chord_notes do
+    --    screen.move(15+w,h*i)
+    --    screen.text(musicutil.note_num_to_name(chord_notes[j]),false)
+    --    w = w+14
+    --  end
+    end
+  end
+  screen.move(0,63)
+  screen.text("scale: "..params:string("scale"))
+  screen.update()
+end
